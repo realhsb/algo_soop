1f7c7ffdf52ff0b19636912079c1fa552fae88c6
realhsb <realhsb@gmail.com>
2024-06-25 20:29:17 +0900

    [Java] BOJ 11660 구간합구하기5

---
+++ algo_soop_java/src/dp/BOJ_11660_구간합구하기5.java
@@ -0,0 +1,76 @@
+package dp;
+
+import java.io.IOException;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class BOJ_11660_구간합구하기5 {
+	
+	static int N, M;
+	static int[][] map;
+	static int[][] xy;
+	static int[][] dp;
+//	static int x1, y1, x2, y2;
+	
+	public static void main(String[] args) throws IOException {
+		setting();
+		StringBuilder sb = new StringBuilder();
+		
+		for (int i = 0; i < M; i++) {
+			sb.append(dp(i)).append("\n");
+		}
+		
+		System.out.println(sb.toString());
+	}
+	
+	static int dp(int m) {
+		int x1 = xy[m][0];
+		int y1 = xy[m][1];
+		int x2 = xy[m][2];
+		int y2 = xy[m][3];
+		
+		int answer = dp[x2][y2] - dp[x1 - 1][y2] - dp[x2][y1 - 1] + dp[x1 - 1][y1 - 1];
+		
+//		System.out.println(dp[x2][y2] + ", " + dp[x1 - 1][y2] + ", " + dp[x2][y1 - 1] + ", " + dp[x1][y1]);
+		
+		return answer;
+	}
+	
+	static void setting() throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		
+		N = Integer.parseInt(st.nextToken());
+		M = Integer.parseInt(st.nextToken());
+		
+		map = new int[N + 1][N + 1];
+		dp = new int[N + 1][N + 1];
+		xy = new int[M][4];
+		
+		// 표에 값 채우기 
+		for (int i = 1; i <= N; i++) {
+			st = new StringTokenizer(br.readLine());
+			for (int j = 1; j <= N; j++) {
+				map[i][j] = Integer.parseInt(st.nextToken());
+			}
+		}
+		
+		// 계산할 x, y 좌표 담기 
+		for (int i = 0; i < M; i++) {
+			st = new StringTokenizer(br.readLine());
+			for (int j = 0; j < 4; j++) {
+				xy[i][j] = Integer.parseInt(st.nextToken());
+			}
+		}
+		
+		// 누적합 구하기 
+		for (int i = 1; i <= N; i++) {
+			for (int j = 1; j <= N; j++) {
+				dp[i][j] = map[i][j] + dp[i - 1][j] + dp[i][j - 1] - dp[i - 1][j - 1];
+//				System.out.print(dp[i][j] + " ");
+			}
+//			System.out.println();
+		}
+	}
+}
